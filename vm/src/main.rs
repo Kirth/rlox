fda2933f541fbfd79efd39e960f1f1a50bfb85a5
Kirth <kirth4ever@gmail.com>
2024-05-02 23:23:01 +0200

    vm: global variables

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -1,9 +1,14 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::collections::HashMap;
 use std::convert::TryFrom;
+use std::fmt::format;
 
 use interpreter::interpreter::{ExprVisitor, Object, StmtVisitor};
 use interpreter::scanner::{self, *};
 use interpreter::parser::*;
+
+use std::fs::File;
+use std::io::{self, Write, Read};
 
 type Value = Object;
 
@@ -12,6 +17,9 @@ type Value = Object;
 enum Opcode {
     Return,
     Constant,
+    Nil, True, False,
+
+    Print,
 
     Add,
     Subtract,
@@ -20,6 +28,14 @@ enum Opcode {
     Negate,
 
     Not,
+    Equal,
+    Greater,
+    Less,
+
+    Pop,
+    DefineGlobal,
+    SetGlobal,
+    GetGlobal,
 }
 
 #[derive(Debug)]
@@ -84,6 +100,7 @@ struct VM {
     chunk: Chunk, // unsure if there's one chunk or multiple
     ip: usize,
     stack: Vec<Value>,
+    globals: HashMap<String, Value>,
     // the clox implementation has a `Value* stackTop;` pointer to the top of the stack
     // they use that as a counter for the current position.  I don't think that would work well in Rust
 }
@@ -91,9 +108,10 @@ struct VM {
 impl VM {
     fn new() -> Self {
         VM {
-            chunk: Chunk { instr: vec![0, 0], constants: vec![ Value::Number(1337.0) ] },
+            chunk: Chunk { instr: vec![], constants: vec![] },
             ip: 0,
-            stack: vec![]
+            stack: vec![],
+            globals: HashMap::new()
         }
     }
 
@@ -119,6 +137,21 @@ impl VM {
         op(a, b)
     }
 
+    // push the result immediately, returning false if failed
+    fn binary_operation_store(&mut self, op: fn(Value, Value) -> Option<Value>) -> bool { 
+        let b = self.pop().unwrap(); // TODO: insufficent bla err?
+        let a = self.pop().unwrap();    
+        let res = op(a, b);
+
+        if res.is_some() {
+            self.push(res.unwrap());
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
     fn run(&mut self) -> Result<(), InterpretError> {
         while true {
             self.chunk.dissasemble_instruction(self.ip);
@@ -127,44 +160,38 @@ impl VM {
 
             match instr {
                 Opcode::Return => {
-                    println!("pop: {:?}", self.pop());
+                    //println!("pop: {:?}", self.pop());
                     return Ok(());
                 },
                 Opcode::Constant => {
                     let constant = self.read_constant()?;
                     self.push(constant);                 
-                }
-                Opcode::Add => { 
-                    if let Some(v) = self.binary_operation(|a, b| {
-                        if a.is_str() || b.is_str() {
+                },
+                Opcode::Nil => self.push(Value::Nil),
+                Opcode::True => self.push(Value::Boolean(true)), // i guess this explains why other rlox implementations do Value::True, Value::False ;3
+                Opcode::False => self.push(Value::Boolean(false)),
+                Opcode::Add => { self.binary_operation_store(|a, b| {
+                        if a.is_string() || b.is_string() {
                             return Some(Value::String(format!("{}{}", a.to_string(), b.to_string())))
                         }
                         
                         Some(Value::Number(a.to_num().unwrap() + b.to_num().unwrap())) 
-                    }) {
-                        self.push(v)
-                    }
+                    });
                 },
                 Opcode::Subtract => { 
-                    if let Some(v) = self.binary_operation(|a, b| {
+                    self.binary_operation_store(|a, b| {
                         Some(Value::Number(a.to_num().unwrap() - b.to_num().unwrap())) 
-                    }) {
-                        self.push(v)
-                    }
+                    });
                 },
                 Opcode::Divide => {
-                    if let Some(v) = self.binary_operation(|a, b| {
+                    self.binary_operation_store(|a, b| {
                         Some(Value::Number(a.to_num().unwrap() / b.to_num().unwrap())) 
-                    }) {
-                        self.push(v)
-                    }
+                    });
                 },
                 Opcode::Multiply => {
-                    if let Some(v) = self.binary_operation(|a, b| {
+                    self.binary_operation_store(|a, b| {
                         Some(Value::Number(a.to_num().unwrap() * b.to_num().unwrap())) 
-                    }) {
-                        self.push(v)
-                    }
+                    });
                 },
                 Opcode::Negate => {
                     let value = Value::Number(- self.pop().unwrap().to_num().unwrap());
@@ -178,6 +205,37 @@ impl VM {
                         Object::Boolean(b) => b,
                         _ => true,
                     }));
+                },
+                Opcode::Print => {
+                    println!("{}", self.pop().unwrap());
+                },
+                Opcode::Equal => {
+                    self.binary_operation_store(|a, b| { Some(Value::Boolean(a == b)) });
+                },
+                Opcode::Less => { // TODO: coerces Num for now
+                    self.binary_operation_store(|a, b| { Some(Value::Boolean(a.to_num().unwrap() < b.to_num().unwrap())) });
+                },
+                Opcode::Greater => { // TODO: coerces Num for now
+                    self.binary_operation_store(|a, b| { Some(Value::Boolean(a.to_num().unwrap() > b.to_num().unwrap())) });
+                },
+                Opcode::Pop => { self.pop(); },
+                Opcode::DefineGlobal => {
+                    let name = self.read_constant()?.as_string().unwrap();
+                    //println!("DefineGlobal read_constant as_string: {}", name);
+                    self.globals.insert(name, self.stack.last().unwrap().clone());
+                    self.pop();
+                },
+                Opcode::GetGlobal => {
+                    let name = self.read_constant()?.as_string().unwrap();
+                    self.push(self.globals.get(&name).unwrap().clone());
+                },
+                Opcode::SetGlobal => {
+                    let name = self.read_constant()?.as_string().unwrap();
+                    if let std::collections::hash_map::Entry::Occupied(mut entry) = self.globals.entry(name.clone()) {
+                        *entry.get_mut() = self.stack.last().unwrap().clone();
+                    } else {
+                        eprintln!("trying to SetGlobal on a non-existant global variable {}", name);
+                    }
                 }
             }
         }
@@ -217,7 +275,17 @@ impl Compiler {
 
 impl ExprVisitor<Result<(), String>> for Compiler {
     fn visit_assign(&mut self, expr: Expr, name: &TokenLoc, value: &Expr) -> Result<(), String> {
-        unimplemented!()
+        for (i, c) in self.chunk.constants.iter().enumerate() {
+            if c.is_string() && c.as_string() == name.token.as_string() {
+                println!("RE-ASSIGNING {:?}", name);
+                value.visit(self).unwrap();
+                self.chunk.emit(Opcode::SetGlobal.into());
+                self.chunk.emit(i as u8);
+                return Ok(())
+            }
+        }
+        println!("Unknown variable name {}", name.token.as_string().unwrap());
+        return Err(format!("Unknown variable name {}", name.token.as_string().unwrap()));
     }
 
     fn visit_binary(&mut self, expr: Expr, left: &Expr, op: &TokenLoc, right: &Expr) -> Result<(), String> {
@@ -226,13 +294,19 @@ impl ExprVisitor<Result<(), String>> for Compiler {
         println!("visiting left expr: {:?}", right);
         right.visit(self);
 
+        println!("with this op: {:?}", op);
+
         match &op.token {
             Token::PLUS => { self.chunk.emit(Opcode::Add.into()) },
             Token::MINUS => { self.chunk.emit(Opcode::Subtract.into()) },
             Token::STAR => { self.chunk.emit(Opcode::Multiply.into()) },
             Token::SLASH => { self.chunk.emit(Opcode::Divide.into()) },
 
-            _ => return Err(format!("Unknown operation {:?}", op))
+            Token::EQ => { self.chunk.emit(Opcode::Equal.into()) },
+            Token::GT => { self.chunk.emit(Opcode::Greater.into()) },
+            Token::LS => { self.chunk.emit(Opcode::Less.into()) },
+
+            _ => { eprintln!("Unknown binary operation {:?}", op); return Err(format!("Unknown binary operation {:?}", op)) }
         }
 
         Ok(())
@@ -257,8 +331,27 @@ impl ExprVisitor<Result<(), String>> for Compiler {
     }
 
     fn visit_literal(&mut self, expr: Expr, value: &Object) -> Result<(), String> {
-        let offset = self.chunk.emit_const_value(value.clone());
-        self.chunk.emit_const(offset);
+
+        /*
+        With number literals, we had to deal with the fact that there are billions of
+        possible numeric values. We attended to that by storing the literal’s value in the
+        chunk’s constant table and emitting a bytecode instruction that simply loaded
+        that constant. We could do the same thing for the new types. We’d store, say,
+        true, in the constant table, and use an OP_CONSTANT to read it out.
+        But given that there are literally (heh) only three possible values we need to
+        worry about with these new types, it’s gratuitous—and slow!—to waste a two
+        byte instruction and a constant table entry on them. Instead, we’ll define three
+        dedicated instructions to push each of these literals on the stack.
+        */
+
+        match &value {
+            Value::Boolean(b) => if *b == true { self.chunk.emit(Opcode::True.into()) } else { self.chunk.emit(Opcode::False.into()) },
+            Value::Nil => self.chunk.emit(Opcode::Nil.into()),
+            _ => {
+                let offset = self.chunk.emit_const_value(value.clone());
+                self.chunk.emit_const(offset);
+            }
+        }
 
         return Ok(())
         //return Err(format!("Uknown Value type {:?}", value));
@@ -299,7 +392,15 @@ impl ExprVisitor<Result<(), String>> for Compiler {
     }
 
     fn visit_variable(&mut self, expr: Expr, name: &TokenLoc) -> Result<(), String> {
-        unimplemented!()
+        for (i, c) in self.chunk.constants.iter().enumerate() {
+            if c.is_string() && c.as_string() == name.token.as_string() {
+                self.chunk.emit(Opcode::GetGlobal.into());
+                self.chunk.emit(i as u8);
+                return Ok(())
+            }
+        }
+
+        return Err(format!("Unknown variable name {}", name.token.as_string().unwrap()));
     }
 
 }
@@ -308,6 +409,7 @@ impl StmtVisitor for Compiler {
     fn visit_expression(&mut self, expr: &Box<Expr>) -> Option<Object> {
         println!("visit_expression: {:?}", expr);
         expr.visit(self);
+        self.chunk.emit(Opcode::Pop.into()); // discard the unused result from an expression such as `brunch = "quiche"; _eat(brunch)_` off the stack
         None
     }
 
@@ -343,7 +445,10 @@ impl StmtVisitor for Compiler {
     }
 
     fn visit_print(&mut self, expr: &Box<Expr>) -> Option<Object> {
-        unimplemented!()
+        expr.visit(self);
+        self.chunk.emit(Opcode::Print.into());
+
+        None
     }
 
     fn visit_return(&mut self, keyword: &TokenLoc, value: &Box<Expr>) -> Option<Object> {
@@ -351,7 +456,25 @@ impl StmtVisitor for Compiler {
     }
 
     fn visit_var(&mut self, name: &TokenLoc, initializer: &Option<Box<Expr>>) -> Option<Object> {
-        unimplemented!()
+        /*
+        Global variables are looked up by name at runtime. That means the VM—the
+        bytecode interpreter loop—needs access to the name. A whole string is too big
+        to stuff into the bytecode stream as an operand. Instead, we store the string in
+        the constant table and the instruction then refers to the name by its index in the table.
+        */
+
+        let idx = self.chunk.emit_const_value(Object::String(name.token.as_string().unwrap()));
+        //println!("Emitted name {} to idx {}", name.token.as_string().unwrap(), idx);
+
+        match initializer {
+            Some(expr) => expr.visit(self).unwrap(),
+            None => self.chunk.emit(Opcode::Nil.into()),
+        }
+        
+        self.chunk.emit(Opcode::DefineGlobal.into());
+        self.chunk.emit(idx);
+
+        None
     }
 
     fn visit_while(&mut self, condition: &Box<Expr>, body: &Box<Stmt>) -> Option<Object> {
@@ -378,14 +501,26 @@ fn compile(source: String) -> Result<Chunk, String> {
     Ok(chunk)
 }
 
+fn save_to_file(chunk: &Chunk, filename: &str) -> io::Result<()> {
+    let mut file = File::create(filename)?;
+    file.write_all(&chunk.instr)?; // oops, what about the consts?
+    Ok(())
+}
+
 fn main() {
     let mut vm = VM::new();
-    vm.chunk = compile("(5 + 5) * 2 + \"foobar\";".to_string()).unwrap();
+    vm.chunk = match compile("var breakfast = \"beignets\"; var beverage = \"cafe au lait\"; breakfast = \"beignets with \" + beverage; print breakfast;".to_string()) {
+        Ok(c) => c,
+        Err(s) => panic!("{}", s),
+    };
+    //vm.chunk = compile("print (5 + 5) * 2 + \"foobar\";".to_string()).unwrap();
 
     println!("{:?}", vm);
 
-
     let res = vm.run();
 
+
     println!("{:?}", res);
+
+    save_to_file(&vm.chunk, "lox_output_program");
 }  
